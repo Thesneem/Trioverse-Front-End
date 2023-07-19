54ff7fcd1aaddc8e9491f13fbc12803afde460e4
Thesneem <thes.bash@gmail.com>
2023-07-19 11:22:18 +0530

    changed config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 
-// export const BASE_URL = 'https://api.trioverse.online'
+export const BASE_URL = 'https://api.trioverse.online'
 
-// export const SOCKET_URL = 'https://api.trioverse.online'
+export const SOCKET_URL = 'https://api.trioverse.online'
 
 
-export const BASE_URL = 'http://localhost:7000'
+// export const BASE_URL = 'http://localhost:7000'
 
-export const SOCKET_URL = 'http://localhost:7000'
+// export const SOCKET_URL = 'http://localhost:7000'
